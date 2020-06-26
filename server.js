b5d68f697bf0b4898b343ed7dc576db69aa8ba54
jefersonjacubosk <67485537+jefersonjacubosk@users.noreply.github.com>
2020-06-26 19:47:12 -0300

    criando uma nova rota "Player"

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const app = express();
 
 var port = process.env.PORT || 80
 
+console.log(__dirname);
+
 app.get('/', (req,res) => {
 res.send('Oi Jeferson');
 
 });
+app.get('/player', (req,res) => {
+    res.send('Oi Player');
+    
+    });
 
 app.listen(port,() => {
 console.log('server iniciando...');
